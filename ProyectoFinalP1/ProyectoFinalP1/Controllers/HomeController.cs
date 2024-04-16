fb030c54fcaaf5634b442e5cd6018cc8506d7aae
K3ury <keury2004@hotmail.com>
2024-04-16 14:26:39 -0400

    Proyect: View Nosotros added

--- ProyectoFinalP1/ProyectoFinalP1/Controllers/HomeController.cs
+++ ProyectoFinalP1/ProyectoFinalP1/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ProyectoFinalP1.Controllers
             return View();
         }
 
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
